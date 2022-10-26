ffd01902146abd860b95c1d222245a08f46e710a
Joaquim Cunha <joaquimafonsocunha@gmail.com>
2022-10-26 14:43:15 +0100

    Adicionado metodos para classe Estudante

--- include/Estudante.h
+++ include/Estudante.h
@@ -13,6 +13,13 @@ private:
 
 public:
     Estudante(int cod_estudante, const std::string &nome);
+    int getCodEstudante() const;
+    std::string getNome() const;
+    std::list<UCTurma> getTurmas() const;
+
+    void addTurma(const UCTurma &turma);
+
+    bool operator==(const Estudante &est) const;
 };
 
 #endif //FEUP_AED1_ESTUDANTE_H
--- src/Estudante.cpp
+++ src/Estudante.cpp
@@ -1,3 +1,23 @@
 #include "../include/Estudante.h"
 
 Estudante::Estudante(int cod_estudante, const std::string &nome): cod_estudante_(cod_estudante), nome_(nome) {}
+
+int Estudante::getCodEstudante() const {
+    return cod_estudante_;
+}
+
+std::string Estudante::getNome() const {
+    return nome_;
+}
+
+std::list<UCTurma> Estudante::getTurmas() const {
+    return turmas_;
+}
+
+void Estudante::addTurma(const UCTurma &turma) {
+    turmas_.push_back(turma);
+}
+
+bool Estudante::operator==(const Estudante &est) const {
+    return (nome_ == est.getNome() && cod_estudante_ == est.getCodEstudante());
+}
